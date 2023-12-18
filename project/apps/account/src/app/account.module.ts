e6703cc382ea5e6dc9f88eff2f64d81c99c77f9f
Куликов Валерий Романович <VRKulikov@beeline.ru>
2023-12-18 19:32:45 +0300

    module3-task1: Добавил библиотеку для конфигурации приложений, добавил .env в gitignore, подключил модуль конфигурации к account

--- project/apps/account/src/app/account.module.ts
+++ project/apps/account/src/app/account.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ConfigAccountModule } from '@project/shared/config';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule],
+  imports: [BlogUserModule, AuthenticationModule, ConfigAccountModule],
   controllers: [],
   providers: [],
   exports: [BlogUserModule],
